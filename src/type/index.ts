ec915841c43bc0564622950ba1a493174b998926
jungmikyung <123jmk@naver.com>
2021-03-03 21:23:25 +0900

    jmk: icon optional 추가

--- src/type/index.ts
+++ src/type/index.ts
@@ -1,30 +1,31 @@
 export type TSize = { width: number, height: number };
 export type TPosition = { lat: number, lng: number };
 export type TPoint = { x: number, y: number };
+
 export type TImageIcon = {
   url: string;
-  size: TSize;
-  scaledSize: TSize;
-  origin: TPoint;
-  anchor: TPoint;
+  size?: TSize;
+  scaledSize?: TSize;
+  origin?: TPoint;
+  anchor?: TPoint;
 };
 
 export type THtmlIcon = {
   content: string | HTMLElement;
-  size: TSize;
-  anchor: TPoint;
+  size?: TSize;
+  anchor?: TPoint;
 };
 
 export type TSymbolIcon = {
   path: string;
-  style: string;
-  radius: string;
-  fillColor: string;
-  fillOpacity: number;
-  strokeColor: string;
-  strokeWeight: number;
-  strokeOpacity: number;
-  anchor: TPoint;
+  style?: string;
+  radius?: string;
+  fillColor?: string;
+  fillOpacity?: number;
+  strokeColor?: string;
+  strokeWeight?: number;
+  strokeOpacity?: number;
+  anchor?: TPoint;
 };
 
 export type TMarkerOptions = {
